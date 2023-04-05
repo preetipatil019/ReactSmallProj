7dd827d4e911a856133e637361243991079351ef
preeti <preetipatil019@gmail.com>
2023-04-05 10:51:47 -0700

    practice tic tac toe

--- src/Square.js
+++ src/Square.js
@@ -4,11 +4,12 @@ import { useState } from "react"
 function Square({value,onSquareClick}) {
     return <button className="square" onClick={onSquareClick}> {value} </button>
 }
-export default function Board() {
-    const [xIsNext, setXIsNext] = useState(true);
-    const [square, setSquare] = useState(Array(9).fill(null))
+
+
+function Board({xIsNext,square,onPlay}) {
+
     function handleClick(i) {
-        if (square[i]) {
+        if (square[i] || calculateWinner(square)) {
             return;
         }
         const nextSquare = square.slice();
@@ -18,11 +19,18 @@ export default function Board() {
              nextSquare[i]="0"
         }
        
-        setSquare(nextSquare)
-        setXIsNext(!xIsNext)
+        onPlay(nextSquare);
      }
- 
+    const winner = calculateWinner(square)
+    let status;
+    if (winner) 
+    {
+        status = "Winner: " + winner;
+    } else {
+        status ="Next Player: "+(xIsNext ?"X" : "0")
+    }
     return (<>
+        <div>{status}</div>
         <div className="board-row">
             <Square value={square[0]} onSquareClick={() => handleClick(0)} />
         <Square  value={square[1]} onSquareClick={() => handleClick(1)} />
@@ -40,5 +48,47 @@ export default function Board() {
         </div>
 
         </>
+    )
+}
+
+function calculateWinner(square) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (square[a] && square[a] === square[b] && square[a] === square[c]) {
+      return square[a];
+    }
+  }
+  return null;
+}
+
+export default function Game() {
+        const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)])
+    const currentSquare = history[history.length - 1];
+
+    function handlePlay(nextSquare)
+    {
+        setHistory([...history, nextSquare])
+        setXIsNext(!xIsNext)
+    }
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} square={currentSquare} onPlay={handlePlay} />
+            </div>
+            <div className="game-info">
+
+            </div>
+        </div>
     )
 }
